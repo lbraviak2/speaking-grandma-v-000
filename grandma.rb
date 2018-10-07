9e1570983f8573c7ad20edcb18289edfd391812f
Lindsay Braviak <lbraviak@gmail.com>
2018-10-07 22:30:46 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,6 @@ def speak_to_grandma(phrase)
 
   if phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif phrase = "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  else
-    puts "HUH?! SPEAK UP, SONNY!"
   end
 
 end
