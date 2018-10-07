99904aedd40a3b11e15b6323e2214aabf255b4c1
Lindsay Braviak <lbraviak@gmail.com>
2018-10-07 22:27:29 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,16 +5,16 @@ def speak_to_grandma(phrase = "Hello")
     puts "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
-  else 
+  else
     puts "HUH?! SPEAK UP, SONNY!"
   end
-  
+
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
